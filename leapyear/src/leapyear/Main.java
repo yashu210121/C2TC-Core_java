f421558e6c9bf2a413faf8776c550dc7f0c9e8d2
Acer <Acer@YASHU21>
2022-05-27 19:01:01 +0530

    leap year

---
+++ leapyear/src/leapyear/Main.java
@@ -0,0 +1,30 @@
+package leapyear;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year=1900;
+		if (year%4==0)
+		{
+			if(year %100==0)
+			{
+				if(year %400==0)
+				{
+					System.out.println("Leap year");
+				}
+				else
+				{
+					System.out.println("not leap year");
+				}
+			}
+			else
+			{
+				System.out.println("Leap year");
+			}
+		}
+
+	}
+
+
+}
