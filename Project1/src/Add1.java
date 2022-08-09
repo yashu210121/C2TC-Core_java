b05fa1932667a2bf41bbe9cfc7673b53cc4996c1
Acer <Acer@YASHU21>
2022-08-09 10:20:35 +0530

    First Commit

---
+++ Project1/src/Add1.java
@@ -0,0 +1,12 @@
+
+public class Add1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c= a+ b;
+		System.out.println(c);
+	}
+
+}
